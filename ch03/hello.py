6cbd113f161be0897a92ee5a3c2496d6659f3b5d
iostate <qmtruong92@gmail.com>
2022-05-29 01:09:20 -0700

    +Routes=["/cookie", "/projects/<project>/<collection>/<name>", "/projects/<project>/<collection>/<name>"]

--- ch03/hello.py
+++ ch03/hello.py
@@ -1,5 +1,5 @@
 import string
-from flask import Flask
+from flask import Flask, abort, make_response, redirect
 app = Flask(__name__)
 
 
@@ -8,10 +8,34 @@ def index():
     return '<h1>Hello World!</h1>', 400
 
 
+#--- REDIRECT
+@app.route('/user/<name>/twitter')
+def redirect_twitter(name):
+    return redirect('http://twitter.com/{}'.format(name))
+# -------
+
+# ----- COOKIES
+
+
+@app.route('/cookie')
+def cookie():
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
+
+# ------
+
+# ----------USERS
+
+
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    if not name:
+        abort(404)
+    return '<h1>Hello, {} sads!</h1>'.format(name)
 
+
+# ----- END USERS
 
 @app.route('/projects/<project>/<collection>/<name>')
 def projects(project, collection, name):
